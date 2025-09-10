76dc1e9215a24e0d6b46b1218b2b83295115c134
Chris Terry <terrychristian26@gmail.com>
2025-09-09 22:18:43 -0400

    modern vulkan debug layers

--- include/vulkan/modern_render_triangle.hpp
+++ include/vulkan/modern_render_triangle.hpp
@@ -1,8 +1,31 @@
-#include <GLFW/glfw3.h>
+#pragma once
 
-#include <iostream>
+// Standard library
 #include <stdexcept>
 #include <cstdlib>
+#include <algorithm>
+#include <memory>
+#include <iostream>
+#include <vector>
+
+// Vulkan
+#ifdef __INTELLISENSE__
+#include <vulkan/vulkan_raii.hpp>
+#else
+import vulkan_hpp;
+#endif
+#include <vulkan/vk_platform.h>
+
+// GLFW
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+// Validation layers
+#ifdef NDEBUG
+constexpr bool VALIDATION_LAYERS = true;
+#else
+constexpr bool VALIDATION_LAYERS = false;
+#endif
 
 class ModernRenderTriangle {
   public:
@@ -23,6 +46,39 @@ class ModernRenderTriangle {
     void initWindow();
 
     /**
+     * @brief create vulkan instance
+     */
+    void createInstance();
+
+    /**
+     * @brief get the required layers
+     * 
+     * @return vector of required layers
+     */
+    std::vector<char const*> getRequiredLayers();
+
+    /**
+     * @brief get list of required ecxtensions based on whether validation layers are enabled
+     * 
+     * @return vector of required extensions
+     */
+    std::vector<char const*> getRequiredExtensions();
+
+    /**
+     * @brief debug callback
+     */
+    static VKAPI_ATTR vk::Bool32 VKAPI_CALL debugCallback(vk::DebugUtilsMessageSeverityFlagBitsEXT severity, vk::DebugUtilsMessageTypeFlagsEXT type, const vk::DebugUtilsMessengerCallbackDataEXT* pCallbackData, void*) {
+        std::cerr << "validation layer: type " << to_string(type) << " msg: " << pCallbackData->pMessage << std::endl;
+
+        return vk::False;
+    }
+
+    /**
+     * @brief setup the debug messenger
+     */
+    void setupDebugMessenger();
+
+    /**
      * @brief main run loop
      */
     void mainLoop();
@@ -32,6 +88,12 @@ class ModernRenderTriangle {
      */
     void cleanup();
 
+    vk::raii::Context                m_context;  ///< vulkan context
+    vk::raii::Instance               m_instance        = nullptr;  ///< vulkan instance
+    vk::raii::DebugUtilsMessengerEXT m_debug_messenger = nullptr;  ///< can have multiple of these
+
     const std::pair<uint32_t, uint32_t> m_window_size = { 800, 800 };
     GLFWwindow*                         m_window;  // GLFW window to render to
+
+    const std::vector<const char*> m_validation_layers = { "VK_LAYER_KHRONOS_validation" };
 };
--- src/vulkan/modern_render_triangle.cpp
+++ src/vulkan/modern_render_triangle.cpp
@@ -1,4 +1,5 @@
 #include "vulkan/modern_render_triangle.hpp"
+#include <cstring>
 
 void ModernRenderTriangle::initWindow() {
     // initialize GLFW without openGL stuff
@@ -11,7 +12,97 @@ void ModernRenderTriangle::initWindow() {
 }
 
 void ModernRenderTriangle::initVulkan() {
+    createInstance();
+    setupDebugMessenger();
+}
 
+void ModernRenderTriangle::setupDebugMessenger() {
+    if (!VALIDATION_LAYERS) {
+        return;
+    }
+
+    // severitys to listen for
+    vk::DebugUtilsMessageSeverityFlagsEXT severityFlags(vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose | vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning | vk::DebugUtilsMessageSeverityFlagBitsEXT::eError);
+    // types to listen for
+    vk::DebugUtilsMessageTypeFlagsEXT    messageTypeFlags(vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral | vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance | vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation);
+    vk::DebugUtilsMessengerCreateInfoEXT debugUtilsMessengerCreateInfoEXT{
+        .messageSeverity = severityFlags,
+        .messageType     = messageTypeFlags,
+        .pfnUserCallback = &debugCallback,  // debug callback function
+        .pUserData       = nullptr
+    };
+
+    m_debug_messenger = m_instance.createDebugUtilsMessengerEXT(debugUtilsMessengerCreateInfoEXT);
+}
+
+std::vector<char const*> ModernRenderTriangle::getRequiredLayers() {
+
+    std::vector<char const*> required_layers;
+    if (VALIDATION_LAYERS) {
+        required_layers.assign(m_validation_layers.begin(), m_validation_layers.end());
+    }
+
+    // check if layers are supported
+    auto layer_properties = m_context.enumerateInstanceLayerProperties();
+    if (std::ranges::any_of(required_layers, [&layer_properties](auto const& required_layer) {
+        return std::ranges::none_of(layer_properties, [required_layer](auto const& layer_property) {
+            return std::strcmp(layer_property.layerName, required_layer) == 0; });
+        })
+
+    ) {
+        throw std::runtime_error("One or more required layers are not supported!");
+    }
+
+    return std::move(required_layers);
+}
+
+std::vector<char const*> ModernRenderTriangle::getRequiredExtensions() {
+    // get GLFW extensions
+    uint32_t glfw_extension_count = 0;
+    auto     glfw_extensions      = glfwGetRequiredInstanceExtensions(&glfw_extension_count);
+
+    // check if extensions are supported by Vulkan
+    auto extension_properties = m_context.enumerateInstanceExtensionProperties();
+    for (uint32_t i = 0; i < glfw_extension_count; ++i) {
+        if (std::ranges::none_of(extension_properties, [glfw_extension = glfw_extensions[i]](auto const& extension_property) { return std::strcmp(extension_property.extensionName, glfw_extension) == 0; })) {
+            throw std::runtime_error("Required GLFW extension " + std::string(glfw_extensions[i]) + " not supported!");
+        }
+    }
+
+    std::vector<char const*> extensions(glfw_extensions, glfw_extensions + glfw_extension_count);
+    if (VALIDATION_LAYERS) {
+        extensions.push_back(vk::EXTDebugUtilsExtensionName);
+    }
+
+    return std::move(extensions);
+}
+
+void ModernRenderTriangle::createInstance() {
+    // vulkan application config
+    constexpr vk::ApplicationInfo app_info{
+        .pApplicationName   = "Modern Triangle Renderer",
+        .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+        .pEngineName        = "No Engine",
+        .engineVersion      = VK_MAKE_VERSION(1, 0, 0),
+        .apiVersion         = vk::ApiVersion14
+    };
+
+    // get required layers
+    std::vector<char const*> required_layers = getRequiredLayers();
+    // get required extensions
+    std::vector<char const*> required_extensions = getRequiredExtensions();
+
+    // instance creation config
+    vk::InstanceCreateInfo create_info {
+        .pApplicationInfo        = &app_info,
+        .enabledLayerCount       = static_cast<uint32_t>(required_layers.size()),
+        .ppEnabledLayerNames     = required_layers.data(),
+        .enabledExtensionCount   = static_cast<uint32_t>(required_extensions.size()),
+        .ppEnabledExtensionNames = required_extensions.data() 
+    };
+
+    // create the instance
+    m_instance = vk::raii::Instance(m_context, create_info);
 }
 
 void ModernRenderTriangle::mainLoop() {
@@ -27,7 +118,10 @@ void ModernRenderTriangle::cleanup() {
 }
 
 void ModernRenderTriangle::run() {
-
+    initWindow();
+    initVulkan();
+    mainLoop();
+    cleanup();
 }
 
 int main() {
